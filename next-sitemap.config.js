9b7b43940c0220edb6f2cc88f6a616070fdeca4b
AMPONSEM1z <allbusiness1z1234@gmail.com>
2025-08-23 16:13:55 +0000

    Added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://bjcleaningservice.netlify.app/", // replace with your Netlify URL
+  generateRobotsTxt: true, // generates robots.txt too
+};
